c5475387e257904e3217ce1a166648b0d2739257
PathiVinayKumar <vinny.pathi@gmail.com>
2026-04-23 17:37:03 +0530

    checking the echo command

--- LearningShell.sh
+++ LearningShell.sh
@@ -1,5 +1,8 @@
 # checking the comments functionality
 echo Vinay # this is inline comment
 ## out side of the block
+echo -e 
 
 ls -lasrsth
+
+echo -e "\e[35m >>>>>>> Install ngix <<<<<<<<<<< \e[0m"
